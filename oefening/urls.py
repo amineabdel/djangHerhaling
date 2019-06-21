a10b5fc07ab7fe4b5dbcbc0ec9858c378b7bbf96
amineabdel <amine.oostende@hotmail.com>
2019-06-21 10:57:00 +0200

    html template from numbers file + path 2 nr in html

--- oefening/urls.py
+++ oefening/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'oefening'
 urlpatterns = [
     path(r'', views.index, name='index'),
-    path(r'<int:line1>/<int:line2>/', views.nummerkes, name='nummerkes'),
-
+    #path(r'<int:line1>/<int:line2>/', views.nummerkes, name='nummerkes')
+    path(r'<int:a>/<int:b>/', views.lijsteke, name='lijsteke')
 ]
--- oefening/views.py
+++ oefening/views.py
@@ -7,6 +7,8 @@ import json
 import random
 
 
+
+
 def index(request):
     file = 'oefening/txtfile/numbers.txt'
     with open(file) as f:
@@ -14,15 +16,30 @@ def index(request):
         return render(request, 'oefening/nrkes.html', context)
 
 def nummerkes(request,line1,line2):
-     return HttpResponse("line1 = %s" % line1)
+    context = {
+        'line1': line1,
+        'line2': line2
+    }
+    return render(request, 'oefening/nrkes.html', context)
 
 
-# def index(request):
-#     return HttpResponse("hallo")
+def lijsteke(request,a,b):
+    lijst = [112,245,14,7,28,200,5386,12,17,11,13478,23,26,18,9999]
+    for i in range(min(lijst),max(lijst)):
+        try:
+            context = {
+                'a': lijst[a],
+                'b': b
+            }
+            return render(request, 'oefening/nrkes.html', context)
+
+        except Exception as e:
+            print(type(e))
 
 
-# def test(request, number):
-#     return HttpResponse("You're looking at question %s." % number)
+
+
+
 
 
 
